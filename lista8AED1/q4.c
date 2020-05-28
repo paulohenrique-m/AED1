d597a7c404ad6be1016f402cda20b321f26a5d0d
Paulo Henrique Marques <paulohenriquemenezes00@gmail.com>
2020-05-28 11:53:41 -0300

    correção de palavras

--- lista8AED1/q4.c
+++ lista8AED1/q4.c
@@ -1,33 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int multiplica(A, B)
-{
-    int z = A;
+int multiplica(A, B) {
+  int z = A;
 
-    if (B == 0 || A == 0)
-        return 0;
-    else if (B == 1)
-        return A;
-    else
-        z += multiplica(A, B - 1);
+  if (B == 0 || A == 0)
+    return 0;
+  else if (B == 1)
+    return A;
+  else
+    z += multiplica(A, B - 1);
 
-    return z;
-    /*
+  return z;
+  /*
 	descrição:função recursiva que retorne o resultado da multiplicação dos dois parâmetros utilizando apenas o operador somas
 	entrada: 2 numeros inteiros
 	saida: resultado da multiplicação em inteiro
 	*/
 }
 
-int main()
-{
-    int A, B;
-    printf("Informe um multiplicando: ");
-    scanf("%d", &A);
-    printf("Informe um multiplicador: ");
-    scanf("%d", &B);
+int main() {
+  int A, B;
+  printf("Informe um multiplicando: ");
+  scanf("%d", &A);
+  printf("Informe um multiplicador: ");
 
-    printf("A * B = %d", multiplica(A, B));
-    return 0;
+  scanf("%d", &B);
+
+  printf("A * B = %d", multiplica(A, B));
+  return 0;
 }
